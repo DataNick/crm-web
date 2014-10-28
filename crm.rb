8df6da23c05c88c788b125bf5343723ceb3a625f
DataNick <DataNick@users.noreply.github.com>
2014-10-28 15:39:01 -0400

    updated contacts with instances

--- crm.rb
+++ crm.rb
@@ -1,11 +1,18 @@
 require_relative 'contact'
 require 'sinatra'
 
+$crm_app_name = "Nick's CRM"
+
 get '/' do
-	@crm_app_name = "Nick's CRM"
+	
 	erb :index
 end
 
 get '/contacts' do
-	erb :contacts
+  @contact = []
+  @contact << Contact.new("Nicholas", "Altobelli", "cricket1fast@gmail.com", "Coding student")
+  @contact << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
+  @contact << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
+
+ erb :contacts
 end
