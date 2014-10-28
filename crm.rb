ccfb3101e2f92f51a21c91c09cb5ed342c2edc1f
DataNick <DataNick@users.noreply.github.com>
2014-10-28 16:02:43 -0400

    fixed up rolodex and contact variables

--- crm.rb
+++ crm.rb
@@ -4,17 +4,17 @@ require_relative 'contact'
 require 'sinatra'
 
 $crm_app_name = "Nick's CRM"
-
+$rolodex = Rolodex.new
 get '/' do
 	
 	erb :index
 end
 
 get '/contacts' do
-  @contact = []
-  @contact << Contact.new("Nicholas", "Altobelli", "cricket1fast@gmail.com", "Coding student")
-  @contact << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
-  @contact << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
+  #@contact = [] this was used because we didn't have a rolodex to store the data.
+  # @contacts << Contact.new("Nicholas", "Altobelli", "cricket1fast@gmail.com", "Coding student")
+  # @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
+  # @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
 
  erb :contacts
 end
