1091af9bec1fa95753027565aa1398deceaacc07
DataNick <DataNick@users.noreply.github.com>
2014-10-31 14:18:14 -0400

    changes to put and delete /contact/:id methods to inlcude DataMapper methods

--- crm.rb
+++ crm.rb
@@ -90,7 +90,7 @@ get "/contacts/:id/edit" do
 end
 
 put "/contacts/:id" do
-  @contact = $rolodex.find(params[:id].to_i)
+    @contact = Contact.get(params[:id].to_i)
   if @contact
     @contact.first_name = params[:first_name]
     @contact.last_name = params[:last_name]
@@ -103,7 +103,7 @@ put "/contacts/:id" do
   end
 
  delete "/contacts/:id" do
-  	@contact = $rolodex.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
   		$rolodex.remove_contact(@contact)
   		redirect to ("/contacts")
